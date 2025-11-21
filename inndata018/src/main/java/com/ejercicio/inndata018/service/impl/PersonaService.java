97f70d44d1c6147f2fb5eaf842a85f18430a3d9d
Alejandro <libre9320@gmail.com>
2025-11-20 22:12:08 -0600

    ia, sonarqube, swagger y h2

--- inndata018/src/main/java/com/ejercicio/inndata018/service/impl/PersonaService.java
+++ inndata018/src/main/java/com/ejercicio/inndata018/service/impl/PersonaService.java
@@ -5,7 +5,6 @@ import com.ejercicio.inndata018.model.request.PersonaRequest;
 import com.ejercicio.inndata018.model.response.PersonaResponse;
 import com.ejercicio.inndata018.repository.PersonaRepository;
 import com.ejercicio.inndata018.service.IPersonaService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,33 +12,29 @@ import java.util.Optional;
 
 @Service
 public class PersonaService implements IPersonaService {
-    @Autowired
-    PersonaRepository personaRepository;
+    private final PersonaRepository personaRepository;
+
+    public PersonaService(PersonaRepository personaRepository) {
+        this.personaRepository = personaRepository;
+    }
+
     @Override
     public List<PersonaResponse> readAll() {
-        List<Persona> listaDePersonas=personaRepository.findAll();
-        List<PersonaResponse> listaFinal= listaDePersonas.stream().map(
-                persona -> {
-                    PersonaResponse personaResponse =new PersonaResponse(persona.getId(),persona.getNombre(), persona.getEdad(),
-                            persona.getIdDepartamento().getId());
-                    return personaResponse;
-
-                })
+        return personaRepository.findAll().stream()
+                .map(persona -> new PersonaResponse(persona.getId(), persona.getNombre(), persona.getEdad(),
+                        persona.getIdDepartamento().getId()))
                 .toList();
-        return listaFinal;
     }
 
     @Override
     public PersonaResponse update(Integer id, PersonaRequest personaRequest) {
         Optional<Persona> persona= personaRepository.findById(id);
-       // PersonaResponse personaResponse= new PersonaResponse();
-        Persona persona1 = new Persona();
 
         if(persona.isPresent()){
-            persona1= persona.get();
+            Persona persona1 = persona.get();
             persona1.setNombre(personaRequest.getNombre());
             persona1.setEdad(personaRequest.getEdad());
-            persona1=personaRepository.save(persona1);
+            persona1 = personaRepository.save(persona1);
 
             return new PersonaResponse(id,persona1.getNombre(),persona1.getEdad(),
                     persona1.getIdDepartamento().getId());
